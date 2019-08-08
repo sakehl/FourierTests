b0fa64fa0d9defead747e0c93a7adbd83be12be2
sakehl <l.b.vandenhaak@gmail.com>
2019-08-09 02:10:47 +1000

    updated script and right acc-llvm commit

--- script.sh
+++ script.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 #BENCHS="Regular"
+date=$(date '+%Y_%m_%d')
+run="time stack run --verbosity silent -- --output results"
+
 #CPU stuff
 # for i in "Regular" "Irregular" "Foreign"
 # do
 #   for j in "1" "100" "1000" "2000" "5000" "10000"
 #   do
-#     time stack run --verbosity silent -- --output results/CPU_bench_20_06_2019_$j_$i.html "CPU/$i/$j" -m "glob"
+#     time stack run --verbosity silent -- --output results/CPU_bench_$date\_$j\_$i.html "CPU/$i/$j" -m "glob"
 #   done
 # done
+
 
 #GPU stuff
 for i in "Regular" "Irregular" "Normal"
 do
   for j in "1" "1000" "10000" "20000" "50000" "100000"
   do
-    time stack run --verbosity silent -- --output results/GPU_bench_20_06_2019_$j_$i.html "GPU/$i/$j" -m "glob"
+    time stack run --verbosity silent -- --output results/GPU_bench_$date\_$j\_$i.html "GPU/$i/$j" -m "glob"
   done
 done
 
@@ -24,7 +28,7 @@ for i in "Normal"
 do
   for j in "1" "100" "1000" "2000"
   do
-    time stack run --verbosity silent -- --output results/CPU_bench_20_06_2019_$j_$i.html "CPU/$i/$j" -m "glob"
+    time stack run --verbosity silent -- --output results/CPU_bench_$date\_$j\_$i.html "CPU/$i/$j" -m "glob"
   done
 done
 
@@ -33,6 +37,6 @@ for i in "Normal"
 do
   for j in "1" "100" "1000" "2000"
   do
-    time stack run --verbosity silent -- --output results/GPU_bench_20_06_2019_$j_$i.html "GPU/$i/$j" -m "glob"
+    time stack run --verbosity silent -- --output results/GPU_bench_$date\_$j\_$i.html "GPU/$i/$j" -m "glob"
   done
 done
