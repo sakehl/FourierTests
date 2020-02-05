9b5ae4ce6dd4324d1c8f2a6d633668d289b7bc60
L.b.vandenhaak@gmail.com <larsvdh@jizo.science.uu.nl>
2020-02-05 13:42:44 +0100

    Updated inputgen

--- Futhark/input_gen.py
+++ Futhark/input_gen.py
@@ -8,12 +8,24 @@ random.seed(42)
 n = int(sys.argv[1])
 m = int(sys.argv[2])
 
-rands = [[random.randint(0, 2**31 - 1) for i in range(n)] for j in range(m)]
+if len(sys.argv) <= 3:
+    rands = [[random.randint(0, 2**31 - 1) for i in range(n)] for j in range(m)]
 
-with open('list_{}_{}.in'.format(str(n),str(m)), 'w') as f:
-    print(rands, file=f)
+    with open('list_{}_{}.in'.format(str(n),str(m)), 'w') as f:
+        print(rands, file=f)
 
-out = list(map(sorted, rands))
+    out = list(map(sorted, rands))
 
-with open('list_{}_{}.out'.format(str(n),str(m)), 'w') as f:
-    print(out, file=f)
+    with open('list_{}_{}.out'.format(str(n),str(m)), 'w') as f:
+        print(out, file=f)
+else:
+    k = int(sys.argv[3])
+    rands = [[[random.randint(0, 2**31 - 1) for i in range(n)] for j in range(m)] for k in range(k)]
+
+    with open('list_{}_{}_{}.in'.format(str(n),str(m),str(k)), 'w') as f:
+        print(rands, file=f)
+
+    out = list(map(sorted, rands))
+
+    with open('list_{}_{}_{}.out'.format(str(n),str(m),str(k)), 'w') as f:
+        print(out, file=f)
